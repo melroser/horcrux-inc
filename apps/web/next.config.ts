9dbcdd7b6f86565fa5944c3188d9ca58fd7a23e6
melroser <themelroser@gmail.com>
2025-10-13 09:51:47 -0400

    adding exlint:

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react']
+  }
+}
 
 export default nextConfig;
